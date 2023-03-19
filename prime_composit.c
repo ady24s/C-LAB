7790c9c3afcc38a9d5efc1a1e8fd85bf666955ae
Adyasha <adyasha24@gmail.com>
2023-03-19 22:59:00 +0530

    2(a)Lab

---
+++ prime_composit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void)
+{
+  int n,i,prime=0;
+  printf ("Enter any number:");
+  scanf ("%d", &n);
+
+  for (i==1; i<=n; i++);
+  {
+    prime++;
+  }
+
+  if (prime==2)
+  printf ("Prime Number");
+
+  else
+  printf("Composite Number");
+
+  return 0;
+}
